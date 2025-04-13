954313cec51929e76bd2b54d37580afa46160521
jim <jim@example.com>
2025-04-14 01:50:43 +0900

    更新Box Formula到版本 1.0.48

--- box.rb
+++ box.rb
@@ -1,8 +1,8 @@
 class Box < Formula
   desc "加密文件夹为单个二进制文件的命令行工具"
   homepage "https://github.com/griffin928/kangaroo-bin"
-  url "https://github.com/griffin928/kangaroo-bin/raw/main/releases/kangaroo-1.0.47.tar.gz"
-  sha256 "0cf9c76643ad8c78ede2a073eaf04721b14db6c07ff15a063935e98b77e2ac5c"
+  url "https://github.com/griffin928/kangaroo-bin/raw/main/releases/kangaroo-1.0.48.tar.gz"
+  sha256 "01f4a37b171d50e11e6c9c47a682c09cec51a03c1290f6facc8071b5fd298073"
   license "专有"
   
   def install
